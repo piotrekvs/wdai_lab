d7a01eb6f3198ae04fd108d7213f605fa2a5ad3b
piotrekvs <pvanselow.2000@poczta.fm>
2021-12-06 12:19:06 +0100

    lab3/zad11 game finished

--- lab3/zad11/script.js
+++ lab3/zad11/script.js
@@ -20,18 +20,28 @@ window.addEventListener('load', () => {
     const timeControl = {
         frameLastTimestamp: 0,
         lastSpownedTimestamp: 0,
+        nextSpownIn: 0,
         clearArray: 0,
     };
     const gameStatus = {
         livesAtStart: 3,
         livesCurrent: 3,
         points: 0,
+        playerName: '',
     };
     const damage = 101;
     const enemyImg = new Image();
     enemyImg.src = 'images/walkingdead.png';
     let enemiesArray = [];
     let isGameOver = true;
+
+    function updateLivesString() {
+        let lives = '';
+        for (let i = 0; i < gameStatus.livesAtStart; i++) {
+            i < gameStatus.livesCurrent ? lives += '\u2764\uFE0F' : lives += '\uD83D\uDDA4';
+        }
+        document.getElementById('ingame-lives').innerText = lives;
+    }
 
     class Enemy {
         constructor() {
@@ -42,6 +52,7 @@ window.addEventListener('load', () => {
             this.sizeFactor = Enemy.#getRandomSizeFactor();
             this.width = Math.floor(this.spriteWidth * this.sizeFactor);
             this.height = Math.floor(this.spriteHeight * this.sizeFactor);
+            this.speed = Enemy.#getRandomSpeed();
             this.frame = 0;
             this.frameLength = 60; // ms, = (1000 / 60) * (4fps)
             this.frameLastTimestamp = 0;
@@ -60,14 +71,21 @@ window.addEventListener('load', () => {
             return screenRes.res[screenRes.current].scale * (0.5 + Math.random());
         }
 
+        static #getRandomSpeed() {
+            return Math.floor(
+                (Math.random() * 5 + 1) * screenRes.res[screenRes.current].scale * 2,
+            );
+        }
+
         update(timestamp) {
             if (this.x + this.width < 0) {
                 if (this.toDelete === false) {
                     gameStatus.livesCurrent--;
+                    updateLivesString();
                 }
                 this.toDelete = true;
             } else {
-                this.x -= 4;
+                this.x -= this.speed;
                 if (timestamp - this.frameLastTimestamp > this.frameLength) {
                     this.frameLastTimestamp = timestamp;
                     this.frame >= (10 - 1) ? this.frame = 0 : this.frame++;
@@ -114,8 +132,50 @@ window.addEventListener('load', () => {
         document.getElementById('div-ui-menu').classList.add('div-ui-hide');
     }
 
-    function showRanking() {
+    async function showRanking(arg) {
         document.getElementById('div-ui-ranking').classList.remove('div-ui-hide');
+        const bestScores = [];
+        // https://jsonblob.com/917356058243252224
+        const url = 'https://jsonblob.com/api/jsonBlob/917356058243252224';
+        await fetch(url, { method: 'GET' }).then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(new Error(`Http error: ${res.status}`));
+        }).then((res) => {
+            res.sort((a, b) => (b.score - a.score)).slice(0, 7)
+                .forEach((val) => bestScores.push(val));
+        }).catch((error) => {
+            // eslint-disable-next-line no-console
+            console.error(error);
+        });
+        if (arg === 'game') {
+            const lastGame = {
+                player: gameStatus.playerName,
+                score: gameStatus.points,
+                date: Date.now(),
+            };
+            bestScores.push(lastGame);
+            await fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(bestScores),
+            }).catch((error) => {
+                // eslint-disable-next-line no-console
+                console.log(error);
+            });
+        }
+        let scoreTable = '';
+        bestScores.sort((a, b) => (b.score - a.score)).forEach((val) => {
+            scoreTable += `<li>${val.player} | Score: ${val.score} | `
+            + `Date: ${new Date(val.date).toDateString()}</li>`;
+        });
+        document.getElementById('ranking-list').innerHTML = scoreTable;
+
+        // eslint-disable-next-line no-console
+        console.log(bestScores);
     }
 
     function hideRanking() {
@@ -135,11 +195,13 @@ window.addEventListener('load', () => {
     }
 
     function handleEnemies(timestamp) {
-        if (timestamp - timeControl.lastSpownedTimestamp > 2000) {
-            // console.log(`timestamp: ${timestamp} | last: ${timeControl.lastSpownedTimestamp}`);
-            // console.log(enemiesArray);
+        if (timestamp - timeControl.lastSpownedTimestamp > timeControl.nextSpownIn) {
             enemiesArray.push(new Enemy());
             timeControl.lastSpownedTimestamp = timestamp;
+            timeControl.nextSpownIn = Math.floor(
+                Math.random() * (3000 / (1 + gameStatus.points / 200)) + 200,
+                // (1 + gameStatus.points / 200) faster when more points
+            );
         }
         if (timestamp - timeControl.clearArray > 10000) {
             enemiesArray = enemiesArray.filter((enemy) => !enemy.toDelete);
@@ -149,12 +211,12 @@ window.addEventListener('load', () => {
 
     function handleHitTest(e) {
         const mouse = {
-            x: e.pageX - divUI.getBoundingClientRect().left,
-            y: e.pageY - divUI.getBoundingClientRect().top,
+            x: e.offsetX,
+            y: e.offsetY,
         };
         let isMissed = true;
         if (!isGameOver) {
-            for (let i = enemiesArray.length - 1; i >= 0; i--) {
+            for (let i = 0; i < enemiesArray.length; i++) {
                 if (enemiesArray[i].hitTest(mouse.x, mouse.y)) {
                     isMissed = false;
                     break;
@@ -169,9 +231,11 @@ window.addEventListener('load', () => {
 
     function gameEnd() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        divUI.removeEventListener('click', handleHitTest);
+        isGameOver = true;
+        enemiesArray = [];
+        divUI.removeEventListener('mousedown', handleHitTest);
         hideIngameUI();
-        showRanking();
+        showRanking('game');
     }
 
     function animate(timestamp) {
@@ -184,7 +248,9 @@ window.addEventListener('load', () => {
             enemiesArray[i].update(timestamp);
             enemiesArray[i].draw();
         }
-        if (!isGameOver) { requestAnimationFrame(animate); } else {
+        if (!isGameOver) {
+            requestAnimationFrame(animate);
+        } else {
             gameEnd();
         }
     }
@@ -192,12 +258,14 @@ window.addEventListener('load', () => {
     function gameStart(e) {
         e.stopPropagation();
         hideMenu();
-        divUI.addEventListener('click', handleHitTest);
+        divUI.addEventListener('mousedown', handleHitTest);
         isGameOver = false;
         enemiesArray = [];
         gameStatus.livesAtStart = 3;
         gameStatus.livesCurrent = 3;
         gameStatus.points = 0;
+        updateLivesString();
+        document.getElementById('ingame-player-name').innerText = gameStatus.playerName;
         document.getElementById('ingame-points').innerText = `POINTS ${gameStatus.points}`;
         showIngameUI();
         animate(0);
@@ -221,11 +289,17 @@ window.addEventListener('load', () => {
     }
 
     function gameUiInit() {
-        document.getElementById('menu-start-game')
-            .addEventListener('click', gameStart);
+        document.getElementById('menu-start-game').addEventListener('click', (e) => {
+            gameStatus.playerName = document.getElementById('menu-player-name').value;
+            if (gameStatus.playerName.length > 0) {
+                gameStart(e);
+            } else {
+                window.alert('Enter player name!');
+            }
+        });
         document.getElementById('menu-ranking').addEventListener('click', () => {
             hideMenu();
-            showRanking();
+            showRanking('');
         });
         document.getElementById('menu-resolution-down')
             .addEventListener('click', () => setResolution(screenRes.current - 1));
@@ -234,6 +308,9 @@ window.addEventListener('load', () => {
         document.getElementById('ranking-close').addEventListener('click', () => {
             hideRanking();
             showMenu();
+        });
+        document.getElementById('ingame-menu').addEventListener('click', () => {
+            isGameOver = true;
         });
     }
 
